3134c975f033f27241dd8f13172c81544737955f
Paul Valle <paul.valle@datadisk.co.uk>
2016-08-03 20:42:25 +0100

    playing around - pfv

--- hello.js
+++ hello.js
@@ -2,8 +2,32 @@
  * Created by p on 01/08/2016.
  */
 
+"use strict";
 
 // Call the console.log function.
 console.log('Hello World');
-
 console.log('goodnight');
+
+var a = 5, b = 4;
+var sum = a + b;
+console.log(sum);
+
+var test = 1;
+if (test) {
+    console.log('Passed')
+}
+else {
+    console.log('Failed')
+}
+
+function hello() {
+    console.log("inside a function");
+}
+hello();
+
+var message = "some string";
+var undef;                          // variable is undefined
+console.log(typeof message);
+console.log(typeof "hello");
+console.log(typeof 95);
+console.log(typeof undef);
