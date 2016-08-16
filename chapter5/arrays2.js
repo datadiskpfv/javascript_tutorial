7660cf1e1e1e595a6b035cb9d263b52b94ce3315
Paul Valle <paul.valle@datadisk.co.uk>
2016-08-16 21:12:12 +0100

    more coverage of arrays - pfv

--- chapter5/arrays2.js
+++ chapter5/arrays2.js
@@ -39,3 +39,27 @@ function compare(value1, value2) {
 numbers2.sort(compare);
 console.log(numbers2);              // thats better we have used a numerical comparison function
 
+// you can concat arrays
+var sport1 = ["football", "ruby", "cricket"];
+var sport2 = sport1.concat();
+console.log(sport2);
+console.log(typeof sport2);
+
+// you can use slice to carve-out elements in an array
+var sport3 = ["football", "ruby", "cricket", "darts", "snooker"];
+var sport4 = sport3.slice(1,3);           // start position, end position but does not include end position
+var sport5 = sport3.slice(2);             // start position then to end of array
+var sport6 = sport3.slice(-2, -1)         // you start from end of array use negative numbers
+var sport7 = sport3.slice(-2)         // last two items
+console.log(sport4);
+console.log(sport5);
+console.log(sport6);
+console.log(sport7);
+
+// you can use splice for deletion, insertion and replacement
+var removed = sport3.splice(0,1);        // remove the first item
+console.log(removed);
+console.log(sport3);
+
+removed = sport3.splice(0, 1, "tennis", "golf");    // remove 1st element, then add elements
+console.log(sport3);
