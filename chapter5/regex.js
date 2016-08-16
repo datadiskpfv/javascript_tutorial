3332b840d7eec2d2652e4ff3aea4a4982edf0bd4
Paul Valle <paul.valle@datadisk.co.uk>
2016-08-16 22:41:10 +0100

    regex - pfv

--- chapter5/regex.js
+++ chapter5/regex.js
@@ -1,3 +1,17 @@
 /**
  * Created by vallep on 8/16/2016.
  */
+
+var reg1 = /cat/g;
+console.log(reg1.test("catastrophe"));    // true
+
+var reg2 = /bat/i;
+console.log(reg2.test("Batty"));          // true
+
+// when you create a regex object there are properties
+// properties: global, ignoreCase, lastIndex, multiline, source
+console.log(reg2.ignoreCase);
+console.log(reg2.multiline);
+console.log(reg2.source);
+
+//
