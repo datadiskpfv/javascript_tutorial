5573bbed5f20bd853a3ab347e7544a72e32cc6b2
Paul Valle <paul.valle@datadisk.co.uk>
2016-09-20 11:16:29 +0100

    playing around with jasmine - pfv

---
+++ karma_jasmine_unit_testing/js/test1.js
@@ -0,0 +1,73 @@
+/**
+ * Created by pvalle on 20/09/2016.
+ */
+
+function inheritPrototype(Driver, Person) {
+    var prototype = Object(Person.prototype);            // create object
+    prototype.constructor = Driver;                        // argument object
+    Driver.prototype = prototype;                          // assign object
+}
+
+// this will be the super class
+function Person(fname, lname) {
+    this.fname = fname;
+    this.lname = lname;
+}
+
+// doing the below allows only one copy for all objects created
+// notice the constructor parameter, this is required to confirm that Person
+// is a Person not a object
+Person.prototype = {
+    constructor: Person,
+    getFirstName: function() { return this.fname; },
+    getLastName: function() { return this.lname; },
+    getName: function() { return this.fname + " " + this.lname; }
+}
+
+// driver will inherit from Person above
+function Driver(fname, lname, cabtype) {
+    // lets steal the constructor
+    Person.call(this, fname, lname);
+    this.cabtype = cabtype ;
+}
+
+// the below makes sure that the proto for Driver points to Person
+Driver.prototype = new Person;
+//inheritPrototype(Driver, Person);
+
+// doing the below allows only one copy for all objects created
+// below is a different way as above
+Driver.prototype.getCabType = function() {
+    return this.cabtype;
+};
+
+var driver1 = new Driver("Paul", "Valle", "Black Cab");
+var driver2 = new Driver("Lorraine", "Valle", "Mini Cab");
+
+console.dir(driver1);
+console.dir(driver2);
+
+console.log("driver1 instanceof Object: " + (driver1 instanceof Object).toString());        // true
+console.log("driver1 instanceof Person: " + (driver1 instanceof Person).toString());        // true
+console.log("driver1 instanceof Driver: " + (driver1 instanceof Driver).toString());        // true
+
+//var mydiv = document.getElementById("mydiv");
+//mydiv.innerHTML = driver1.getName() + " drives a " + driver1.getCabType() + "<br>" +
+//    driver2.getName() + " drives a " + driver2.getCabType();
+
+// added function to test jasmine
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// left these variable undefined as a test for jasmine
+var undef1;
+var undef2 = null;
+var undef3 = "";
+
+var def1 = true;
+var def2 = "Hello World!";
--- karma_jasmine_unit_testing/tests/validate_tests.js
+++ karma_jasmine_unit_testing/tests/validate_tests.js
@@ -27,4 +27,77 @@ describe("The name validator", function(){
         });
     });
 
+    // check add function in test1.js
+    describe("Check Math functions", function() {
+
+        it("Check add function", function() {
+            expect(add(4,6)).toEqual(10);
+            expect(add(4,7)).not.toEqual(10);
+        });
+
+        it("Check multiply function", function() {
+            expect(multiply(5,5)).toEqual(25);
+            expect(multiply(10,10)).toEqual(100);
+            expect(multiply(5,5)).not.toEqual(26);
+        })
+    });
+
+    // checking test1.js script file
+    describe("Checks test1.js Objects (Person, Driver)", function() {
+
+        beforeEach(function() {
+            person1 = new Person("Paul", "Valle");
+            driver1 = new Driver("Paul", "Valle", "Black Cab");
+            driver2 = new Driver("Paul", "Valle", "Black Cab");
+        });
+
+        it("Check the Person object", function() {
+            expect(person1.getFirstName()).toEqual("Paul");
+            expect(person1.getLastName()).toEqual("Valle");
+
+            expect(driver1.getFirstName()).toContain("Paul");
+        });
+
+        it("Check the Driver object", function() {
+            expect(driver1.getFirstName()).toEqual("Paul");
+            expect(driver1.getLastName()).toEqual("Valle");
+            expect(driver1.getCabType()).toEqual("Black Cab");
+        });
+
+        it("Check Objects equal", function() {
+            // checking if objects are actually equal
+            expect(driver1).toEqual(driver1);
+            expect(driver1).toBe(driver1);
+            expect(driver1).not.toBe(driver2);
+        });
+
+        it("Check Driver instanceof Object, Person and Driver", function() {
+           expect(driver1 instanceof Object).toBe(true);
+           expect(driver1 instanceof Person).toBe(true);
+           expect(driver1 instanceof Driver).toBe(true);
+        });
+
+        it("Check undefined variables", function() {
+            expect(undef1).toBeFalsy();
+            expect(undef2).toBeFalsy();
+            expect(undef3).toBeFalsy();
+
+            // be more specific
+            expect(undef1).toBeUndefined();
+            expect(undef2).toBeNull();
+        });
+
+        it("Check defined variables", function() {
+            expect(def1).toBeTruthy();
+            expect(def2).toBeTruthy();
+
+            // bit more specific
+            expect(def1).toBeDefined();
+        });
+
+        it("Check that a string is contained", function() {
+            expect(def2).toContain("World");
+            expect(driver1.getFirstName()).toContain("Paul");
+        });
+    });
 });
