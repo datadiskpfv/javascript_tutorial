079ed007420f918bddab3fce675267ccfad2fd55
Paul Valle <paul.valle@datadisk.co.uk>
2016-09-21 13:12:38 +0100

    added mark and watch location - pfv

--- geolocation/myLoc.js
+++ geolocation/myLoc.js
@@ -6,7 +6,11 @@ window.onload = getMyLocation;
 
 function getMyLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+        //navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+        var watchButton = document.getElementById("watch");
+        watchButton.onclick = watchLocation;
+        var clearWatchButton = document.getElementById("clearWatch");
+        clearWatchButton.onclick = clearWatch;
     } else {
         alert("Oops, no geolocation support");
     }
@@ -17,12 +21,18 @@ function displayLocation(position) {
     var longitude = position.coords.longitude;
     var div = document.getElementById("location");
     div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+    div.innerHTML += " (with " + position.coords.accuracy + " meters accuracy)";
 
     var km = computeDistance(position.coords, ourCoords);
     var distance = document.getElementById("distance");
     distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
 
-    showMap(latitude, longitude);
+    if (map == null) {
+        showMap(latitude, longitude);
+    }
+   else {
+        scrollMapToPosition(latitude, longitude);
+    }
 }
 
 function displayError(error) {
@@ -70,14 +80,61 @@ var orbCoords = {
 var map;
 
 function showMap(latitude, longitude) {
+
+    var googleLatAndLong = new google.maps.LatLng(latitude, longitude);
+
     var mapOptions = {
         // going to code orb coords as working on PC (no GPS)
-        center: new google.maps.LatLng(51.516912, -0.08379),
+        center: new google.maps.LatLng(51.516912, -0.08345),
         //center: new google.maps.LatLng(latitude, longitude),
-        zoom: 18,
+        zoom: 15,
         // Map types: HYBRID ROADMAP SATELLITE TERRAIN
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var mapDiv = document.getElementById("map");
     var map = new google.maps.Map(mapDiv, mapOptions);
+
+    var title = "Your Location";
+    var content = "You are here: " + latitude + ", " + longitude;
+    addMarker(map, googleLatAndLong, title, content);
 }
+
+function addMarker(map, latlong, title, content) {
+    var markerOptions = {
+        position: latlong,
+        map: map,
+        title: title,
+        clickable: true
+    };
+    var marker = new google.maps.Marker(markerOptions);
+
+    var infoWindowOptions = {
+        content: content,
+        position: latlong
+    };
+    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+    google.maps.event.addListener(marker, "click", function() {infoWindow.open(map);});
+}
+
+var watchId = null;
+function watchLocation() {
+    watchId = navigator.geolocation.watchPosition(displayLocation,
+        displayError);
+}
+
+function clearWatch() {
+    if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+    }
+}
+
+function scrollMapToPosition(latitude, longitude) {
+    var latitude = latitude;
+    var longitude = longitude;
+    var latlong = new google.maps.LatLng(latitude, longitude);
+    map.panTo(latlong);
+    addMarker(map, latlong, "Your new location", "You moved to: " +
+        latitude + ", " + longitude);
+}
+
