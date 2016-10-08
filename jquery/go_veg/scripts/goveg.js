abefdeb29bb03f86fa0ce62de154017ceeaabdd9
Paul Valle <paul.valle@datadisk.co.uk>
2016-10-08 09:37:07 +0100

    jquery basics - pfv

--- jquery/go_veg/scripts/goveg.js
+++ jquery/go_veg/scripts/goveg.js
@@ -6,15 +6,38 @@ $(document).ready(function() {
     var v = false;
     $("button#vegOn").click(function() {
         if ( v == false) {
-            v = true;
+          // remove the .fish class but save it in a variable so we can restore later
+          $f = $(".fish").parent().parent().detach();
+
+          // lets change hamburgers for portobello mushrooms
+          $(".hamburger").replaceWith("<li class='portobello'><em>Portobello Mushroom</em></li>");
+
+          // replace meat class with tofu class, insert tofu class then detach the meat class
+          $(".meat").after("<li class='tofu'><em>Tofu</em></li>");
+          $(".tofu").parent().parent().addClass("veg_leaf");
+
+          $m = $(".meat").detach();
+          v = true;
         }
-        $f = $(".fish").parent().parent().detach();
-        alert(JSON.stringify($f[0]));
     });
 
     $("button#restoreMe").click(function() {
         if ( v == true) {
-            v = false;
+          // restore the fish class
+          $(".menu_entrees li").first().before($f);
+
+          // restore the hamburgers by replacing the portobello mushroom
+          $(".portobello").replaceWith("<li class='hamburger'>hamburger</li>");
+
+          // restore the meat class
+          $(".tofu").each(function(i) {
+              $(this).after($m[i]);
+          });
+
+          // now we can remove the tofu class and the veg_leaf class
+          $(".tofu").parent().parent().removeClass("veg_leaf");
+          $(".tofu").remove();
+              v = false;
         }
     });
 })
